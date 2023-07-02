3bd712c670c526805fc4489feb5a2a17c425f191
Salaah Amin <salaah01@users.noreply.github.com>
2023-07-02 17:29:45 +0100

    file rearragenemtns

--- adapters/nodejs/benchmark.js
+++ adapters/nodejs/benchmark.js
@@ -1,10 +1,46 @@
-var fs = require('fs');
-var filePath = "/home/salaah/json-lineage/sample_data/sample.json";
-var stream = fs.createReadStream(filePath, 'utf8');
-stream.on('data', function (chunk) {
-    var json = JSON.parse(chunk);
-    console.log(json);
-});
-stream.on('error', function (err) {
-    console.error('Error reading file:', err);
-});
+const fs = require("fs");
+const _process = require("process");
+
+const FILE_PATH = "/home/salaah/json-lineage/sample_data/500kb_sample.json";
+
+const withStream = () => {
+  const stream = fs.createReadStream(FILE_PATH, "utf8");
+
+  stream.on("data", (chunk) => {
+    JSON.parse(chunk);
+  });
+
+  stream.on("error", (err) => {
+    console.error("Error reading file:", err);
+  });
+};
+
+const withReadFile = () => {
+  fs.readFile(FILE_PATH, "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return;
+    }
+
+    JSON.parse(data);
+  });
+};
+
+
+const benchmark = (fn) => {
+  const start = performance.now();
+  const mem_start = _process.memoryUsage().heapUsed;
+  fn();
+  const end = performance.now();
+  const mem_end = _process.memoryUsage().heapUsed;
+  const mem_used_mb = (mem_end - mem_start) / 1024 / 1024;
+
+  console.log(`${fn.name}: ${end - start}ms, ${mem_used_mb}MB`);
+};
+
+const main = () => {
+//   benchmark(withStream);
+  benchmark(withReadFile);
+};
+
+main();
