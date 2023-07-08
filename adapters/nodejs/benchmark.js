171b70bc66c40c78ee1de50aeb1bf708773e5dca
Salaah Amin <salaah01@users.noreply.github.com>
2023-07-08 13:17:52 +0100

    fixed broken benchmark

--- adapters/nodejs/benchmark.js
+++ adapters/nodejs/benchmark.js
@@ -1,13 +1,14 @@
 const fs = require("fs");
 const _process = require("process");
 
-const FILE_PATH = "/home/salaah/json-lineage/sample_data/500kb_sample.json";
+const FILE_PATH = "/home/salaah/json-lineage/sample_data/324mb_sample.json";
 
 const withStream = () => {
   const stream = fs.createReadStream(FILE_PATH, "utf8");
 
   stream.on("data", (chunk) => {
-    JSON.parse(chunk);
+    let s = JSON.parse(chunk);
+    console.log(s);
   });
 
   stream.on("error", (err) => {
@@ -15,31 +16,24 @@ const withStream = () => {
   });
 };
 
-const withReadFile = () => {
-  fs.readFile(FILE_PATH, "utf8", (err, data) => {
-    if (err) {
-      console.error("Error reading file:", err);
-      return;
-    }
+const withReadFile = async () => {
 
-    JSON.parse(data);
-  });
+  const data = await fs.promises.readFile(FILE_PATH, "utf8");
 };
 
 
-const benchmark = (fn) => {
+const benchmark = async (fn) => {
   const start = performance.now();
   const mem_start = _process.memoryUsage().heapUsed;
-  fn();
+  await fn();
   const end = performance.now();
   const mem_end = _process.memoryUsage().heapUsed;
   const mem_used_mb = (mem_end - mem_start) / 1024 / 1024;
 
-  console.log(`${fn.name}: ${end - start}ms, ${mem_used_mb}MB`);
+  console.log(`${fn.name}: ${(end - start).toFixed(2)} ms, ${mem_used_mb.toFixed(2)} MB`);
 };
 
 const main = () => {
-//   benchmark(withStream);
   benchmark(withReadFile);
 };
 
